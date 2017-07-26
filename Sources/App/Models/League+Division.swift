0805b05990ba053859bba91d871bbd5fc19192be
Kyle McMahon <kyle.mcmahon@mac.com>
2017-07-25 21:32:07 -0400

    Add division

--- Sources/App/Models/League+Division.swift
+++ Sources/App/Models/League+Division.swift
@@ -2,16 +2,25 @@ import Vapor
 import PostgreSQLProvider
 
 final class League: Model {
-    var leagueId: Int
     let storage = Storage()
 
     init(row: PostgreSQLProvider.Row) throws {
-        leagueId = try row.get("league_id")
     }
 
     func makeRow() throws -> PostgreSQLProvider.Row {
         var row = PostgreSQLProvider.Row()
-        try row.set("league_id", leagueId)
+        return row
+    }
+}
+
+final class Division: Model {
+    let storage = Storage()
+
+    init(row: PostgreSQLProvider.Row) throws {
+    }
+
+    func makeRow() throws -> PostgreSQLProvider.Row {
+        var row = PostgreSQLProvider.Row()
         return row
     }
 }

0610643747f033cd812536ce72fe223b84833fa1
Kyle McMahon <kyle.mcmahon@mac.com>
2017-07-26 22:50:05 -0400

    Get MLB enums from lib

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor-community/postgresql-provider.git", majorVersion: 2)
+        .Package(url: "https://github.com/vapor-community/postgresql-provider.git", majorVersion: 2),
+        .Package(url: "https://github.com/kamcma/mlb-swift.git", majorVersion: 0)
     ],
     exclude: [
         "Config"

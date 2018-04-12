70e85bb31ae12e08127affb2aa1aaec223818890
Kyle McMahon <kyle.mcmahon@mac.com>
2018-04-11 20:31:40 -0400

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "MLBStandings",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc")
     ],
     targets: [
         .target(name: "MLBAPI", dependencies: []),
